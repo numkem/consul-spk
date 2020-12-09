fff5e3066e823fe348547f6d68bcb2c628543505
Nathan A. Ferch <nf@marginal.net>
2020-12-08 21:32:12 -0500

    automatically download consul

--- create_spk.sh
+++ create_spk.sh
@@ -1,4 +1,15 @@
-#!/bin/sh
+#!/bin/sh -e
+
+ARCH=amd64
+OS=linux
+
+(
+  rm 1_create_package/consul/* consul.zip consul || true
+  . 2_create_project/INFO
+  wget https://releases.hashicorp.com/consul/$version/consul_${version}_${OS}_${ARCH}.zip -O consul.zip
+  unzip consul.zip
+  mv consul 1_create_package/consul
+)
 
 cd 1_create_package 
 tar cvfz package.tgz * 
